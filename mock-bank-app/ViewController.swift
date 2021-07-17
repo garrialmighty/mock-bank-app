ce9797083ddc1f5f6a36ca99978db83087697662
Garri Adrian Nablo <garri.almighty@gmail.com>
2021-07-17 10:47:33 +0800

    Initial Commit

---
+++ mock-bank-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mock-bank-app
+//
+//  Created by Garri Nablo on 17/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
